1dcc7493d2daf2ce5ffe164f0e8b31d83f325bd8
Gemin Pak <geminpak@gmail.com>
2023-09-06 10:23:06 -0700

    first commit

---
+++ navbar.js
@@ -0,0 +1,315 @@
+// get elements
+let menuLink = $(".menu_dp-link");
+let tabLink = $(".menu_dropdown-tab-item");
+let content = $(".menu_dropdown_content");
+let tabContent = $(".menu_tab-content-wrap");
+let menuBG = $(".menu_bg");
+let dropdownWrap = $(".menu_content");
+gsap.defaults({
+  duration: 0.2,
+});
+
+function revealDropdown(currentLink, currentContent) {
+  console.log("REVEAL");
+  dropdownWrap.css("display", "flex");
+  let linkText = currentLink.find(".menu_link-text");
+  let linkTextOffset = linkText.offset().left - 20;
+
+  // Set the initial width of menuBG
+  gsap.set(menuBG, {
+    width: currentContent.outerWidth(),
+    height: currentContent.outerHeight(),
+    x: linkTextOffset,
+  });
+
+  // Update the x transform of menuBG
+  gsap.to(menuBG, {
+    x: linkTextOffset,
+    //width: currentContent.outerWidth(),
+    // height: currentContent.outerHeight(),
+    //duration: 0.2,
+  });
+
+  gsap.set(currentContent, {
+    opacity: 0,
+    display: "none",
+  });
+  gsap.set(currentContent, {
+    opacity: 1,
+    x: "0em",
+    display: "flex",
+  });
+}
+
+function switchDropdown(currentLink, previousContent, currentContent) {
+  let linkText = currentLink.find(".menu_link-text");
+  let linkTextOffset = linkText.offset().left - 20;
+
+  // invert moveDistance if needed
+  let moveDistance = 10;
+  if (currentContent.index() < previousContent.index()) {
+    moveDistance = moveDistance * -1;
+  }
+
+  gsap.to(menuBG, {
+    x: linkTextOffset,
+    width: currentContent.outerWidth(),
+    height: currentContent.outerHeight(),
+    duration: 0.2,
+  });
+
+  gsap.fromTo(
+    previousContent,
+    { opacity: 1, x: "0em", display: "flex" },
+    {
+      opacity: 0,
+      x: moveDistance * -1 + "em",
+      duration: 0.2,
+      display: "none",
+      onComplete: function () {
+        previousContent.removeClass("active");
+        gsap.set(previousContent, { display: "none" });
+      },
+    }
+  );
+
+  gsap.fromTo(
+    currentContent,
+    { opacity: 0, x: moveDistance + "em", display: "none" },
+    {
+      opacity: 1,
+      x: "0em",
+      duration: 0.2,
+      display: "flex",
+      onComplete: function () {
+        // Update the width of menuBG after the animation has completed
+        gsap.to(menuBG, {
+          width: currentContent.outerWidth(),
+          duration: 0.2,
+        });
+      },
+    }
+  );
+}
+
+let isFirstReveal = true; // Add this variable to keep track of the first reveal
+
+menuLink.on("mouseenter", function () {
+  let previousLink = menuLink.filter(".active").removeClass("active");
+  let currentLink = $(this).addClass("active");
+  let previousContent = content.filter(".active").removeClass("active");
+  let currentContent = content.eq($(this).index()).addClass("active");
+  let relatedCaret = $(this).find(".menu_dropdown-caret");
+
+  gsap.killTweensOf(content);
+
+  gsap.to(relatedCaret, { rotation: 180, duration: 0.2 });
+  // Set the display property of all dropdown content to "none"
+  gsap.set(content, { display: "none" });
+  // Set the display property of the currentContent to "flex"
+  gsap.set(currentContent, { display: "flex" });
+
+  // Update the x transform of menuBG
+  gsap.to(menuBG, {
+    width: currentContent.outerWidth(),
+    height: currentContent.outerHeight(),
+    duration: 0.2,
+  });
+
+  showDropdown.play();
+
+  if (previousLink.length === 0) {
+    revealDropdown(currentLink, currentContent);
+  } else if (previousLink.index() !== currentLink.index()) {
+    switchDropdown(currentLink, previousContent, currentContent);
+  }
+});
+
+menuLink.on("mouseleave", function () {
+  let currentLink = $(this);
+  let currentContent = content.eq(currentLink.index()).removeClass("active");
+
+  // Check if the dropdown content is being hovered over
+  if (!dropdownWrap.is(":hover")) {
+    gsap.killTweensOf(content);
+    // Set the display property of the previousContent to "none"
+    gsap.set(currentContent, { display: "none" });
+    showDropdown.reverse();
+    gsap.to(".menu_dropdown-caret", { rotation: 0, duration: 0.2 });
+    menuLink.removeClass("active");
+  }
+});
+
+// Open dropdown animation
+let showDropdown = gsap.timeline({
+  onStart: function () {
+    let currentLink = menuLink.filter(".active");
+    let linkText = currentLink.find(".menu_link-text");
+    let linkTextOffset = linkText.offset().left - 20;
+    let currentContent = content.eq(currentLink.index());
+    gsap.killTweensOf(content);
+
+    // Update the x transform of menuBG
+    gsap.to(menuBG, {
+      x: linkTextOffset,
+      width: currentContent.outerWidth(),
+      height: currentContent.outerHeight(),
+      duration: 0.2,
+    });
+  },
+  paused: true,
+  onReverseComplete: () => {
+    dropdownWrap.css("display", "none");
+    menuLink.removeClass("active");
+  },
+});
+showDropdown.from(dropdownWrap, { opacity: 0, rotateX: -10, duration: 0.2 });
+
+// Menu Hover Out
+dropdownWrap.on("mouseleave", function () {
+  gsap.killTweensOf(content);
+  showDropdown.reverse();
+  gsap.to(".menu_dropdown-caret", { rotation: 0, duration: 0.2 }); // rotate the caret back
+});
+
+function revealTab(currentLink, currentContent) {
+  dropdownWrap.css("display", "flex");
+
+  gsap.set(tabContent, {
+    opacity: 0,
+  });
+  gsap.set(currentContent, {
+    opacity: 1,
+    y: "0em",
+  });
+}
+
+function switchTab(currentLink, previousContent, currentContent) {
+  // invert moveDistance if needed
+  let moveDistance = 2;
+  if (currentContent.index() < previousContent.index()) {
+    moveDistance = moveDistance * -1;
+  }
+
+  gsap.fromTo(
+    previousContent,
+    { opacity: 1, y: "0em" },
+    {
+      opacity: 0,
+      y: moveDistance * -1 + "em",
+      duration: 0.2,
+    }
+  );
+  gsap.fromTo(
+    currentContent,
+    { opacity: 0, y: moveDistance + "em" },
+    {
+      opacity: 1,
+      y: "0em",
+      duration: 0.2,
+    }
+  );
+}
+
+// Open tab animation
+let showTab = gsap.timeline({
+  paused: true,
+  onReverseComplete: () => {
+    dropdownWrap.css("display", "none");
+    tabLink.removeClass("active");
+  },
+});
+showTab.from(dropdownWrap, { opacity: 0, rotateX: -10, duration: 0.2 });
+
+// Tab Hover Out
+$(".menu_dropdown-tab-wrap").on("mouseleave", function () {
+  showTab.reverse();
+});
+
+// Tab Link Hover In
+tabLink.on("mouseenter", function () {
+  // get elements
+  let previousLink = tabLink.filter(".active").removeClass("active");
+  let currentLink = $(this).addClass("active");
+  let previousContent = tabContent.filter(".active").removeClass("active");
+  let currentContent = tabContent.eq($(this).index()).addClass("active");
+  // play animations
+  if (previousLink.length === 0) {
+    revealTab(currentLink, currentContent);
+  } else if (previousLink.index() !== currentLink.index()) {
+    switchTab(currentLink, previousContent, currentContent);
+  }
+});
+
+// Mobile Menu
+
+// Open mobile menu
+$(".w-nav-button").on("click", function () {
+  setTimeout(function () {
+    if ($(".w-nav-overlay").is(":visible")) {
+      // Add the 'modal-open' class to the body element
+      $("body").addClass("menu-open");
+      // Add the 'container-scroll' class to the specific container
+      $(".nav_mobile-menu").addClass("container-scroll");
+    } else {
+      // Reverse the actions
+      $("body").removeClass("menu-open");
+      $(".nav_mobile-menu").removeClass("container-scroll");
+    }
+  }, 1000);
+});
+
+function animateMenu(clickClass, animateClass) {
+  $(clickClass).on("click", function () {
+    // Toggle the 'active' class
+    $(animateClass).toggleClass("active");
+    $(".logo").hide();
+    $(".nav_back-link").css("display", "flex");
+
+    // Use GSAP to animate the menus
+    gsap.to(".nav_mobile-main", {
+      x: "-100%",
+      duration: 0.4,
+      ease: "power2.out",
+    });
+    gsap.fromTo(
+      animateClass,
+      { x: "100%" },
+      { x: "0%", duration: 0.4, ease: "power2.out" }
+    );
+  });
+
+  $(".nav_container").on("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+  });
+
+  // Back link click
+  $(".nav_back-link, .toggle-menu").on("click", function (event) {
+    $("[class^='nav_mobile-']").removeClass("active");
+    // Prevent the default action from closing menu
+    event.preventDefault();
+    event.stopPropagation();
+    // Toggle the 'active' class
+    //$(animateClass).toggleClass("active");
+    $(".logo").show();
+    $(".nav_back-link").hide();
+
+    // Use GSAP to animate the menus
+    gsap.to(".nav_mobile-main", {
+      x: "0%",
+      duration: 0.4,
+      ease: "power2.out",
+    });
+    gsap.to(animateClass, {
+      x: "100%",
+      duration: 0.4,
+      ease: "power2.out",
+    });
+  });
+}
+
+animateMenu(".nav_sublink-item.conditions", ".nav_mobile-conditions");
+animateMenu(".nav_sublink-item.locations", ".nav_mobile-locations");
+animateMenu(".nav_sublink-item.insurance", ".nav_mobile-insurance");
+animateMenu(".nav_link.resources", ".nav_mobile-resources");
