622e962c81d4a542ecb47790b2093a3f0418b402
Gemin Pak <geminpak@gmail.com>
2023-12-12 13:30:48 -0800

    Remove observe for swipers

--- conditions-landing.js
+++ conditions-landing.js
@@ -38,106 +38,91 @@ $(".faq_content-wrapper .condition_rich-text p").each(function () {
 });
 
 function initializeSwiper() {
-  // Create a new IntersectionObserver instance
-  var observer = new IntersectionObserver(function (entries) {
-    // Loop over the entries
-    entries.forEach(function (entry) {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Load the Swiper script
-        loadScript(
-          "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
-          function () {
-            $(".condition_stories-component").each(function (index) {
-              const swiper = new Swiper($(this).find(".swiper")[0], {
-                slidesPerView: 1,
-                speed: 600,
-                spaceBetween: 64,
-                initialSlide: 0,
-                slideToClickedSlide: true,
-                centeredSlides: false,
-                loop: true,
-                slideActiveClass: "is-active",
-                slideDuplicateActiveClass: "is-active",
-                keyboard: false,
-                disableOnInteraction: false,
-                pagination: {
-                  el: ".swiper_pagination-wrapper",
-                  bulletElement: "div",
-                  bulletClass: "swiper_pagination-bullet",
-                  bulletActiveClass: "is-active",
-                  clickable: true,
-                },
-                navigation: {
-                  nextEl: "#story-right",
-                  prevEl: "#story-left",
-                },
-                breakpoints: {
-                  0: {
-                    /* when window >=0px - webflow mobile landscape/portrait */
-                  },
-                  480: {
-                    /* when window >=0px - webflow mobile landscape/portrait */
-                  },
-                  767: {
-                    /* when window >= 767px - webflow tablet */ spaceBetween: 32,
-                  },
-                  992: {
-                    /* when window >= 988px - webflow desktop */
-                  },
-                },
-              });
-            });
-          }
-        );
-
-        // Stop observing the target element
-        observer.unobserve(entry.target);
-      }
-    });
-  });
-
-  // Start observing an element
-  observer.observe(document.querySelector(".section_stories"));
-
-  $(".related-articles_component").each(function (index) {
-    const swiper = new Swiper($(this).find(".swiper")[0], {
-      slidesPerView: 3,
-      speed: 600,
-      spaceBetween: 64,
-      initialSlide: 0,
-      slideToClickedSlide: true,
-      centeredSlides: false,
-      loop: true,
-      slideActiveClass: "is-active",
-      slideDuplicateActiveClass: "is-active",
-      keyboard: false,
-      disableOnInteraction: false,
-      pagination: {
-        el: ".swiper_pagination-wrapper",
-        bulletElement: "div",
-        bulletClass: "swiper_pagination-bullet",
-        bulletActiveClass: "is-active",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: "#story-right",
-        prevEl: "#story-left",
-      },
-      breakpoints: {
-        0: {
-          /* when window >=0px - webflow mobile landscape/portrait */
-        },
-        480: {
-          /* when window >=0px - webflow mobile landscape/portrait */
-        },
-        767: {
+  // Load the Swiper script
+  loadScript(
+    "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
+    function () {
+      $(".condition_stories-component").each(function (index) {
+        const swiper = new Swiper($(this).find(".swiper")[0], {
           slidesPerView: 1,
-        },
-        992: {
-          /* when window >= 988px - webflow desktop */
-        },
-      },
-    });
-  });
+          speed: 600,
+          spaceBetween: 64,
+          initialSlide: 0,
+          slideToClickedSlide: true,
+          centeredSlides: false,
+          loop: true,
+          slideActiveClass: "is-active",
+          slideDuplicateActiveClass: "is-active",
+          keyboard: false,
+          disableOnInteraction: false,
+          pagination: {
+            el: ".swiper_pagination-wrapper",
+            bulletElement: "div",
+            bulletClass: "swiper_pagination-bullet",
+            bulletActiveClass: "is-active",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: "#story-right",
+            prevEl: "#story-left",
+          },
+          breakpoints: {
+            0: {
+              /* when window >=0px - webflow mobile landscape/portrait */
+            },
+            480: {
+              /* when window >=0px - webflow mobile landscape/portrait */
+            },
+            767: {
+              /* when window >= 767px - webflow tablet */ spaceBetween: 32,
+            },
+            992: {
+              /* when window >= 988px - webflow desktop */
+            },
+          },
+        });
+      });
+
+      $(".related-articles_component").each(function (index) {
+        const swiper = new Swiper($(this).find(".swiper")[0], {
+          slidesPerView: 3,
+          speed: 600,
+          spaceBetween: 64,
+          initialSlide: 0,
+          slideToClickedSlide: true,
+          centeredSlides: false,
+          loop: true,
+          slideActiveClass: "is-active",
+          slideDuplicateActiveClass: "is-active",
+          keyboard: false,
+          disableOnInteraction: false,
+          pagination: {
+            el: ".swiper_pagination-wrapper",
+            bulletElement: "div",
+            bulletClass: "swiper_pagination-bullet",
+            bulletActiveClass: "is-active",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: "#story-right",
+            prevEl: "#story-left",
+          },
+          breakpoints: {
+            0: {
+              /* when window >=0px - webflow mobile landscape/portrait */
+            },
+            480: {
+              /* when window >=0px - webflow mobile landscape/portrait */
+            },
+            767: {
+              slidesPerView: 1,
+            },
+            992: {
+              /* when window >= 988px - webflow desktop */
+            },
+          },
+        });
+      });
+    }
+  );
 }
