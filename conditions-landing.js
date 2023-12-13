f832281232cee67eef1b0a071dc3a2c1d80b1c50
Gemin Pak <geminpak@gmail.com>
2023-12-13 15:53:54 -0800

    remove load scripts from conditions and local

--- conditions-landing.js
+++ conditions-landing.js
@@ -1,30 +1,3 @@
-// Load in all Finsweet Libraries for Find Provider Component & initialize Swiper
-window.onload = function () {
-  setTimeout(function () {
-    loadScript(
-      "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsload@1/cmsload.js",
-      function () {}
-    );
-    loadScript(
-      "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsnest@1/cmsnest.js",
-      function () {}
-    );
-    loadScript(
-      "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js",
-      function () {}
-    );
-    loadScript(
-      "https://cdn.jsdelivr.net/npm/@finsweet/attributes-scrolldisable@1/scrolldisable.js",
-      function () {}
-    );
-    loadScript(
-      "https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorclick@1/mirrorclick.js",
-      function () {}
-    );
-  }, 2000);
-  initializeSwiper();
-};
-
 $(".faq_title").each(function () {
   var text = $(this).text();
   var updatedText = text.replace("<Name>", name);
