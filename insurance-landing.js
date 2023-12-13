cab97122dc098f8ed051999da33b3e151ea85ad8
Gemin Pak <geminpak@gmail.com>
2023-12-13 15:21:37 -0800

    added finsweet callback

--- insurance-landing.js
+++ insurance-landing.js
@@ -25,7 +25,23 @@ window.onload = function () {
   //setTimeout(function () {
   loadScript(
     "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsload@1/cmsload.js",
-    function () {}
+    function () {
+      window.fsAttributes = window.fsAttributes || [];
+      window.fsAttributes.push([
+        "cmsload",
+        (listInstances) => {
+          console.log("cmsload Successfully loaded!");
+
+          // The callback passes a `listInstances` array with all the `CMSList` instances on the page.
+          const [listInstance] = listInstances;
+
+          // The `renderitems` event runs whenever the list renders items after switching pages.
+          listInstance.on("renderitems", (renderedItems) => {
+            console.log(renderedItems);
+          });
+        },
+      ]);
+    }
   );
   loadScript(
     "https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsnest@1/cmsnest.js",
