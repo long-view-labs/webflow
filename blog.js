5f628d5ca02cb1052dc601a4679bca3f94cf2964
Gemin Pak <geminpak@gmail.com>
2024-03-13 11:39:13 -0700

    Update token for location

--- blog.js
+++ blog.js
@@ -178,7 +178,7 @@ var blogPostRtbText = document.querySelector(".blog-post-rtb").textContent;
 // Check if the text includes 'local'
 if (blogPostRtbText.includes("{{local}}")) {
   // Call the IPInfo API
-  const apiKey = "89320a08dbdfa6";
+  const apiKey = "22652a4d0b5237";
   const apiURL = `https://ipinfo.io?token=${apiKey}`;
   $.ajax({
     url: apiURL,
